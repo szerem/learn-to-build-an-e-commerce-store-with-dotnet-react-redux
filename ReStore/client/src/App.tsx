0b6afc3dfbc6df1b4f61bf9df1e8c76deeabfba9
Krzysztof <szerem@users.noreply.github.com>
2022-03-11 16:41:58 +0100

    beasic react

--- ReStore/client/src/App.tsx
+++ ReStore/client/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const initializeProducts = [
+  { id: 0, name: 'product1', price: 100.0 },
+  { id: 1, name: 'product2', price: 200.0 },
+];
+
+const App = () => {
+  // console.log(`App ${new Date().toISOString()} ...`);
+
+  const [products, setProducts] = useState(initializeProducts);
+
+  useEffect(() => {
+    console.log('useEffect ...');    
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())      
+      .then((response) => setProducts(response))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const addProduct = () => {
+    setProducts((prev) => [
+      ...products,
+      { id: prev.length + 1, name: `product ${prev.length + 1}`, price: 300.0 },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={item.id}>
+            {index + 1}) {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   );
-}
+};
 
 export default App;
