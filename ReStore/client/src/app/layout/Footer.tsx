bb8978ced3051f06284159001f4f4557cc8859b5
Krzysztof <szerem@users.noreply.github.com>
2022-03-29 13:37:44 +0200

    footer

--- ReStore/client/src/app/layout/Footer.tsx
+++ ReStore/client/src/app/layout/Footer.tsx
@@ -1,14 +1,15 @@
 import { Typography } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
 import ButtonMailto from './ButtonMailto';
 
 const Footer = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {`Copyright © ${new Date().getFullYear()} `}
-      <ButtonMailto label="szerem™" email="szerem@gmail.com"></ButtonMailto>
-    </Typography>
+    <footer className="footer">
+      <Typography variant="body2" color="text.secondary" align="center">
+        {`Copyright © ${new Date().getFullYear()} `}
+        <ButtonMailto label="szerem" email="szerem@gmail.com"></ButtonMailto>
+        {`™`}
+      </Typography>
+    </footer>
   );
 };
 
