fd82ef427e5761ec94c9f4827c2303bcfe56d643
Krzysztof <szerem@users.noreply.github.com>
2022-04-08 11:18:49 +0200

    Setting up the axios get request to use our params

--- ReStore/client/src/app/api/agent.ts
+++ ReStore/client/src/app/api/agent.ts
@@ -54,14 +54,14 @@ axios.interceptors.response.use(
 );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
+  get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
   post: (url: string, body: {}) => axios.post(url).then(responseBody),
   put: (url: string, body: {}) => axios.put(url).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalog = {
-  list: () => requests.get('products'),
+  list: (params: URLSearchParams) => requests.get('products', params),
   details: (id: number) => requests.get(`products/${id}`),
   fetchFilters: () => requests.get('products/filters'),
 };
